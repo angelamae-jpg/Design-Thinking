9eb122c26d557ac5899ecab4d2582b149091514b
angelamae-jpg <angelamaeborja1@gmail.com>
2026-03-20 00:31:35 +0800

    Update service-worker.js
    gyg

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,10 @@
 self.addEventListener("install", function(e) {
   e.waitUntil(
-    caches.open("todo-list-cache").then(function(cache) {
-      return cache.addAll(["index.html"]);
+    caches.open("mindlift-cache").then(function(cache) {
+      return cache.addAll([
+        "index.html",
+        "manifest.json"
+      ]);
     })
   );
 });
